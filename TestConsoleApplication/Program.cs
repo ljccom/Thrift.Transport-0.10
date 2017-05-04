ea98d5ce081ca45b59cc8e11db80aaebda107330
lijinchuan <403479851@qq.com>
2017-05-04 17:57:39 +0800

    测试代码

--- TestConsoleApplication/Program.cs
+++ TestConsoleApplication/Program.cs
@@ -25,8 +25,8 @@ namespace TestConsoleApplication
                 transport.Open();
 
                 Console.Write("已连接上");
-                client.disableTable(Encoding.UTF8.GetBytes("dz_CDN_Ip_Stat"));
-                client.deleteTable(Encoding.UTF8.GetBytes("dz_CDN_Ip_Stat"));
+                //client.disableTable(Encoding.UTF8.GetBytes("dz_CDN_Ip_Stat"));
+                //client.deleteTable(Encoding.UTF8.GetBytes("dz_CDN_Ip_Stat"));
                 //client.createTable(Encoding.UTF8.GetBytes("dz_CDN_Ip_Stat"), new List<ColumnDescriptor> { new ColumnDescriptor { Name = Encoding.UTF8.GetBytes("col1") } });
                 //Console.Write("创建表");
                 
@@ -34,20 +34,36 @@ namespace TestConsoleApplication
                 //Console.WriteLine("写数据:" + boo);
 
                 //根据表名，RowKey名来获取结果集
-                List<TRowResult> reslut = client.getRow(Encoding.UTF8.GetBytes("dz_CDN_Ip_Stat"), Encoding.UTF8.GetBytes("row1"), null);
-                Console.WriteLine("读数据:" + reslut.Count);
+                //List<TRowResult> reslut = client.getRow(Encoding.UTF8.GetBytes("dz_CDN_Ip_Stat"), Encoding.UTF8.GetBytes("row1"), null);
+                //Console.WriteLine("读数据:" + reslut.Count);
 
-                //遍历结果集
-                foreach (var key in reslut)
+                ////遍历结果集
+                //foreach (var key in reslut)
+                //{
+                //    Console.WriteLine("RowKey:\n{0}", Encoding.UTF8.GetString(key.Row));
+                //    //打印Qualifier和对应的Value
+                //    foreach (var k in key.Columns)
+                //    {
+                //        Console.WriteLine("Family:Qualifier:" + "\n" + Encoding.UTF8.GetString(k.Key));
+                //        Console.WriteLine("Value:" + Encoding.UTF8.GetString(k.Value.Value));
+                //    }
+                //}
+
+                var tables = client.getTableNames();
+
+                foreach (var bi in tables)
                 {
-                    Console.WriteLine("RowKey:\n{0}", Encoding.UTF8.GetString(key.Row));
-                    //打印Qualifier和对应的Value
-                    foreach (var k in key.Columns)
+                    var tb = Encoding.UTF8.GetString(bi);
+                    Console.WriteLine(tb);
+                    transport.Close();
+                    transport.Open();
+                    var dic = client.getColumnDescriptors(bi);
+                    foreach (var kv in dic)
                     {
-                        Console.WriteLine("Family:Qualifier:" + "\n" + Encoding.UTF8.GetString(k.Key));
-                        Console.WriteLine("Value:" + Encoding.UTF8.GetString(k.Value.Value));
+                        Console.WriteLine(string.Format("{0}:{1}", Encoding.UTF8.GetString(kv.Key), Encoding.UTF8.GetString(kv.Value.Name)));
                     }
                 }
+
             }
             catch (Exception e)
             {
